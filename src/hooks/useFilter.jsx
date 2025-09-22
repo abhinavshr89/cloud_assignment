b5efd162622f4919fcc92cfe033267725e031b98
Abhinav <abhinav312003@gmail.com>
2025-09-22 23:20:09 +0530

    Update useFilter.jsx

--- src/hooks/useFilter.jsx
+++ src/hooks/useFilter.jsx
@@ -1,6 +1,5 @@
 import { useContext } from 'react';
 import { FilterContext } from '../context/FilterContext';
-
 export const useFilter = () => {
   const context = useContext(FilterContext);
   if (!context) {
